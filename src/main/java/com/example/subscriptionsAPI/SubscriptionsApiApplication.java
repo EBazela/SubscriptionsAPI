09fa486db4f1bde1f958bf83399b113ad7c11e8c
eryk <erykb8031999@gmail.com>
2021-07-02 12:22:52 +0200

    git init

---
+++ src/main/java/com/example/subscriptionsAPI/SubscriptionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionsAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/subscriptionsAPI/SubscriptionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionsAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
